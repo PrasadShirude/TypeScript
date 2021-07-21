99a350ff5431c6abc89207cf9249600b1cbad6c6
prasad_9689 <84795268+PrasadShirude@users.noreply.github.com>
2021-07-21 12:27:33 +0530

    Add files via upload

---
+++ Data.ts
@@ -0,0 +1,91 @@
+/**
+var data:string = "Jay Ganesh.."
+console.log(data)
+
+var isDone : boolean = false
+console.log(isDone)
+
+var num : number = 11
+console.log(num)
+
+var arr:number[] = [11,21,51];
+console.log(arr)
+
+var brr:any[] = [1,true,"free"];
+console.log(brr[1])
+
+// Union types
+function printId(id: number | string)
+{
+    if(typeof id == "string")
+    {
+        console.log(id.toUpperCase());
+    }
+    else
+    {
+        console.log(id);
+    }
+}
+
+printId(11)
+
+
+function welcomePeople(x: string[]|string)
+{
+    if(Array.isArray(x))
+    {
+        console.log("Hello "+x[0]+x.join(" and "));
+    }
+    else
+    {
+        console.log("Welcome lone traveller "+x);
+    }
+}
+var x:string[] = ['Prasad','Ramesh','Shirude'];
+//var x :number[] = [1,2,3];
+//var x:string = "Prasad"
+welcomePeople(x)
+
+
+let myVar:any;
+
+myVar = "ganesh";
+
+console.log(myVar);
+
+
+var isValid:boolean = false;
+
+var message = isValid ? "isvalid" : "is not valid";
+console.log(message);
+
+////
+class Character
+{
+    fullname : string;
+
+    constructor(firstname:string,lastname:string)
+    {
+        this.fullname = firstname +" "+lastname;
+    }
+
+    greet(name? : string)
+    {
+        if(name)
+        {
+            return "Hi " +name+"! my name is "+this.fullname;
+        }
+        else
+        {
+            return "Hi my name is "+this.fullname;
+        }
+    }
+}
+var spark = new Character("Prasad","Shirude");
+var msg = spark.greet();
+console.log(msg);
+msg = spark.greet("Peace");
+console.log(msg);
+////
+
+*/
